0fd4befc9449916ee3a084e0d3eba2fa24e7992b
Rubinha14101505 <134503850+Rubinha14101505@users.noreply.github.com>
2023-08-22 09:15:38 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,32 @@
+ function somaValores(){
+var s1 = document.getElementById("valor1").value;
+var s2 = document.getElementById("valor2").value; 
+var s3 = parseInt(s1) + parseInt (s2);
+
+alert(s3);
+     }
+
+     function subtraiValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt (s1) - parseInt (s2);
+
+ alert(s3);
+     }
+
+     function multiplicaValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt (s1) * parseInt (s2);
+
+ alert(s3);
+     }
+
+     function divideValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt (s1) / parseInt (s2);
+
+ alert(s3);
+
+     }
